0d01b1faea71208ca43bba82c3c8544c411c2c4f
14240422-Allan-Thang <allan.thang1920@gmail.com>
2022-09-26 18:55:13 +1000

    Created base Map to start BSP.

--- AGP_A3/Source/AdvGamesProgramming/DungeonGenerator.cpp
+++ AGP_A3/Source/AdvGamesProgramming/DungeonGenerator.cpp
@@ -9,13 +9,23 @@ ADungeonGenerator::ADungeonGenerator()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	RoomCount = 10;
+	RoomSize_Min = 10;
+	RoomSize_Max = 100;
+
+	MapSize = 100 * RoomSize_Max * RoomCount;
+
 }
 
 // Called when the game starts or when spawned
 void ADungeonGenerator::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	Map.MinPos = FVector(0.0f, 0.0f,0.0f);
+	Map.MaxPos = Map.MinPos + FVector(MapSize, MapSize, 0);
+	Map.Width = Map.MaxPos.X / 100;
+	Map.Height = Map.MaxPos.Y / 100;
 }
 
 // Called every frame
@@ -25,3 +35,24 @@ void ADungeonGenerator::Tick(float DeltaTime)
 
 }
 
+void ADungeonGenerator::GenerateMap()
+{
+}
+
+void ADungeonGenerator::MakeRoom()
+{
+}
+
+void ADungeonGenerator::SpawnTiles()
+{
+}
+
+void ADungeonGenerator::BSP_SplitRoom_Vert(Room RoomToSplit)
+{
+	int SplitAtPercent = FMath::RandRange(1, 10);
+}
+
+void ADungeonGenerator::BSP_SplitRoom_Hor(Room RoomToSplit)
+{
+	int SplitAtPercent = FMath::RandRange(1, 10);
+}
--- AGP_A3/Source/AdvGamesProgramming/DungeonGenerator.h
+++ AGP_A3/Source/AdvGamesProgramming/DungeonGenerator.h
@@ -15,12 +15,41 @@ public:
 	// Sets default values for this actor's properties
 	ADungeonGenerator();
 
+	struct Room
+	{
+		FVector MinPos;
+		FVector MaxPos;
+
+		float Width;
+		float Height;
+
+		Room* Parent;
+		Room* Child1;
+		Room* Child2;
+	};
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
+	int RoomCount;
+	int RoomSize_Max;
+	int RoomSize_Min;
+
+	TArray<FVector> FloorTiles;
+
+	Room Map;
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	void GenerateMap();
+	void MakeRoom();
+	void SpawnTiles();
+	void BSP_SplitRoom_Vert(Room RoomToSplit);
+	void BSP_SplitRoom_Hor(Room RoomToSplit);
+
+private:
+	int MapSize;
 
 };
